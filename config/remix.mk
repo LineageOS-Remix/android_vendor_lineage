06ea72bf9eea7e7de8f726933de9ffb841cf7805
ShevT <evgeny.shishkov@gmail.com>
2024-08-30 12:38:33 +0300

    config: Adding an option to build DeviceAsWebcam
    Change-Id: I6aad72a339a2c619ef9f99ccdb17e876b91c474f

--- config/remix.mk
+++ config/remix.mk
@@ -1,3 +1,12 @@
 # Inherit common additions
 $(call inherit-product, vendor/lineage/config/art.mk)
 $(call inherit-product, vendor/gapps/arm64/arm64-vendor.mk)
+
+# DeviceAsWebcam
+ifeq ($(TARGET_BUILD_DEVICE_AS_WEBCAM), true)
+    PRODUCT_PACKAGES += \
+        DeviceAsWebcam
+
+    PRODUCT_VENDOR_PROPERTIES += \
+        ro.usb.uvc.enabled=true
+endif
