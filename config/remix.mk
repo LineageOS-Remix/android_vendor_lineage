3811f110c2388c23837a64591dc0068d41cd7ee3
Danny Lin <danny@kdrag0n.dev>
2025-02-15 18:56:13 +0800

    config: Disable touch video heatmap
    Change-Id: Iadec34fe1eb574bf40cf4a1f849772e5f64ee8db
    Signed-off-by: rmp22 <195054967+rmp22@users.noreply.github.com>

--- config/remix.mk
+++ config/remix.mk
@@ -14,3 +14,8 @@ endif
 # Disable default frame rate limit for games
 PRODUCT_PRODUCT_PROPERTIES += \
     debug.graphics.game_default_frame_rate.disabled=true
+
+# Disable touch video heatmap to reduce latency, motion jitter, and CPU usage
+# on supported devices with Deep Press input classifier HALs and models
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.input.video_enabled=false
