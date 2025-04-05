08324072167f8fbe5fadab38c4be574de8a915e5
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-04-05 13:25:32 +0800

    config: Add bypass charging prop
    Change-Id: Iee2e99e9ec929221336fea361ab516b00f47bb7e

--- config/remix.mk
+++ config/remix.mk
@@ -45,3 +45,8 @@ else
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.custom.blur.enable=false
 endif
+
+# Bypass Charging
+BYPASS_CHARGE_SUPPORTED ?= false
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.battery_bypass_supported=$(BYPASS_CHARGE_SUPPORTED)
