9a3e15394e249abc7a3c0b52a3da5aa1cc2bdcbc
CuriousNom <aryanbiswas7585@gmail.com>
2026-05-05 13:37:15 +0530

    build: Add support to generate fastboot packages
    * Just run 'brunch device img' or 'brunch device img userdebug'
    
    Change-Id: I3ba5e0993c9b9656c622b4e8d7c7ef6ddbca6143

--- build/tasks/bacon.mk
+++ build/tasks/bacon.mk
@@ -27,3 +27,16 @@ $(LINEAGE_TARGET_PACKAGE): $(INTERNAL_OTA_PACKAGE_TARGET)
 
 .PHONY: bacon
 bacon: $(LINEAGE_TARGET_PACKAGE) $(DEFAULT_GOAL)
+
+# -----------------------------------------------------------------
+# Lineage fastboot image package
+
+LINEAGE_IMG_PACKAGE := $(PRODUCT_OUT)/lineage-$(LINEAGE_VERSION)-img.zip
+
+$(LINEAGE_IMG_PACKAGE): $(INTERNAL_UPDATE_PACKAGE_TARGET)
+	$(hide) ln -f $(INTERNAL_UPDATE_PACKAGE_TARGET) $(LINEAGE_IMG_PACKAGE)
+	$(hide) $(SHA256) $(LINEAGE_IMG_PACKAGE) | sed "s|$(PRODUCT_OUT)/||" > $(LINEAGE_IMG_PACKAGE).sha256sum
+	@echo "IMG Package: $(LINEAGE_IMG_PACKAGE)" >&2
+
+.PHONY: bacon-img
+bacon-img: $(DEFAULT_GOAL) $(LINEAGE_IMG_PACKAGE)
