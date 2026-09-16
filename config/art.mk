265cd3430abf0deb3673351752b2b345b7bbf996
Abhay Singh Gill <abhaygill017@gmail.com>
2026-09-16 18:01:25 +0530

    config: art: Update boot-image-profile.txt path for A15 QPR2
    * frameworks/base/config/boot-image-profile.txt now moved to frameworks/base/boot/boot-image-profile.txt
    
    Change-Id: I61af081b402f6e63722a5ae9e22151227a713c95
    Signed-off-by: Aeoniixx <aeoniixx@gmail.com>
    Signed-off-by: Abhay Singh Gill <abhaygill017@gmail.com>

--- config/art.mk
+++ config/art.mk
@@ -14,7 +14,7 @@ PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := speed-profile
 
 # Boot image profile
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # System server compiler
 PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
