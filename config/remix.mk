6c30fd359eacbbfcb61443a269b48bc08ced39a8
ponces <ponces26@gmail.com>
2026-09-23 20:23:46 +0530

    config: Build AOSPA's ParanoidSense
    Change-Id: Iffab29b0a6f2767e07921dd6d860035dad43ee4f

--- config/remix.mk
+++ config/remix.mk
@@ -19,3 +19,15 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # on supported devices with Deep Press input classifier HALs and models
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.input.video_enabled=false
+
+# ParanoidSense
+ifneq ($(TARGET_FACE_UNLOCK_SUPPORTED),false)
+PRODUCT_PACKAGES += \
+    FaceUnlock
+
+PRODUCT_SYSTEM_EXT_PROPERTIES += \
+    ro.face.sense_service=true
+
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.biometrics.face.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/android.hardware.biometrics.face.xml
+endif
